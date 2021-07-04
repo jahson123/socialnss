ddf0b6a53a2722ed3ea883d0dbf9b2e73ae641ed
jahson123 <74171442+jahson123@users.noreply.github.com>
2021-07-04 21:50:52 +0800

    Update post.js

--- static/js/post.js
+++ static/js/post.js
@@ -1,38 +1,60 @@
 video = ['mp4', 'mov', 'avi', 'webm'];
 img = ['gif','png','jpg','jpeg'];
+var result = "" ;
+var a,b;
 
 document.getElementById("file").addEventListener("change", function(e) {
     var files = e.target.files;
     var view = document.getElementById("view");
+    a = b = result = "";
     if (files.length <= 6 ) {
-        for ( i=0; i<files.length; i++) {
-            var reader = new FileReader();
-            var file = files[i];
-            type = file.name.split(".");
-            if (img.includes(type[type.length-1])) {
-                reader.addEventListener("load", function(e) {
-                    var data = e.target;
-                    var img = document.createElement("img");
-                    img.classList.add("image");
-                    img.src= data.result;
-                    view.insertBefore(img, null);
-                });
+        for (j=0; j<files.length; j++) {
+            type = files[j].name.split(".")
+            if (Number(files[j].size/1024/1024) > Number(30)) {
+                var a = files[j].name;
+                result = true;
             }
-           else if (video.includes(type[type.length-1])) {
-                reader.addEventListener("load", function(e) {
-                    var data = e.target;
-                    var vid = document.createElement("video");
-                    vid.classList.add("video");
-                    vid.src= data.result;
-                    view.insertBefore(vid, null);
-                });
+            else if (type[type.length-1] == "mp3") {
+                var b = files[j].name;
+                result = true;
             }
-            reader.readAsDataURL(file);
         }
-        document.querySelector("#view").style.display = "inline";
-        document.getElementsByClassName("upload_file")[0].style.display = "none";
-        document.querySelector("input[type='submit']").disabled = false;
-        document.querySelector(".err_message").style.display = "none";
+        if ( result == "" ) {
+            for ( i=0; i<files.length; i++) {
+                var reader = new FileReader();
+                var file = files[i];
+                type = file.name.split(".");
+                if (img.includes(type[type.length-1])) {
+                    reader.addEventListener("load", function(e) {
+                        var data = e.target;
+                        var img = document.createElement("img");
+                        img.classList.add("image");
+                        img.src= data.result;
+                        view.insertBefore(img, null);
+                    });
+               }
+               else if (video.includes(type[type.length-1])) {
+                    reader.addEventListener("load", function(e) {
+                        var data = e.target;
+                        var vid = document.createElement("video");
+                        vid.classList.add("video");
+                        vid.src= data.result;
+                        view.insertBefore(vid, null);
+                    });
+               }
+               reader.readAsDataURL(file);
+            }
+            document.querySelector("#view").style.display = "inline";
+            document.getElementsByClassName("upload_file")[0].style.display = "none";
+            document.querySelector("input[type='submit']").disabled = false;
+            document.querySelector(".err_message").style.display = "none";
+        }
+        else if ( result == true ) {
+            if ( a != "" ) { alert("You cannot upload ''" + a + "'' that is more than 30mb."); }
+            else if ( b != "" ) { alert("You cannot upload ''" + b  + "'' that is  mp3 file."); }
+            document.querySelector("input[type='file']").value = "";
+            document.querySelector("#view").style.display = "none";
+        }
     }
     else {
         document.querySelector(".err_message").style.display = "inline";
