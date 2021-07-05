661d152884321b30775cb1e0dbc74109c1e6d7a3
jahson123 <74171442+jahson123@users.noreply.github.com>
2021-07-05 21:16:44 +0800

    Update slideshow_post.js

--- static/js/slideshow_post.js
+++ static/js/slideshow_post.js
@@ -1,13 +1,24 @@
 var slideIndex = 1;
+
+function vidpause() {
+    var vid = document.getElementsByTagName("video");
+    for (z=0; z<vid.length; z++) {
+        vid[z].pause();
+    }
+}
+
+
 document.querySelectorAll(".slide")[0].style.display = "block";
 document.querySelector(".next").addEventListener("click", function() {
     var n = 1;
     showSlides(slideIndex += n);
+    vidpause();
 });
 
 document.querySelector(".prev").addEventListener("click", function() {
     var n = -1;
     showSlides(slideIndex += n);
+    vidpause();
 });
 
 function showSlides(n) {
